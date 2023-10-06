6613ccefb1edd1f89c5af3b13faff4bcd0d2ab36
SAMi-Rn <sami_roudgarian@yahoo.com>
2023-10-05 20:19:17 -0700

    fsm.c remove

--- socket_fsm/cmake-build-debug/cmake_install.cmake
+++ socket_fsm/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/samiroudgarian/CLionProjects/socket_fsm
+# Install script for directory: /Users/samiroudgarian/CLionProjects/fsm/socket_fsm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/samiroudgarian/CLionProjects/socket_fsm/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/samiroudgarian/CLionProjects/fsm/socket_fsm/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
